ce9adf6e241f022701a91031c7e4b0519b57b07a
Alexei Capsamun <caps.alex@mail.ru>
2023-07-07 16:51:29 +0300

    Added _008 _009 _010 _011

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 54, 51, 84, 87, 63, 11, 87};
+
+int n = array.Length;
+int find = 87;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
